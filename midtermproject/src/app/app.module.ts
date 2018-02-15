c945791c584729740cb39d5195cbaef068a1fa1a
ahsirt101197 <banua.gabrielle10@gmail.com>
2018-02-16 05:01:23 +0800

    added login page and style

--- midtermproject/src/app/app.module.ts
+++ midtermproject/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
